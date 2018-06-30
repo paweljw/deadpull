25ee4db24c5badf8d53e3b867b6d72ab1287b8f0
Paweł J. Wal <pawel.wal@codesthq.com>
2018-06-30 23:32:31 +0200

    Fix mishap in Values::S3Client spec

--- spec/deadpull/values/s3_client_spec.rb
+++ spec/deadpull/values/s3_client_spec.rb
@@ -2,9 +2,16 @@
 
 RSpec.describe Deadpull::Values::S3Client do
   let(:config) { { aws: { region: 'fakeregion' } } }
+
   subject { described_class.concretize(config) }
+  
+  let(:client) { double }
+
+  before do
+    allow(Aws::S3::Client).to receive(:new).and_return(client)
+  end
 
   describe '#concretize' do
-    it { expect(subject).to be_a Aws::S3::Client }
+    it { expect(subject).to eq client }
   end
 end
